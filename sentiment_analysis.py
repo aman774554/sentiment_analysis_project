a34955ca73af30f9f5f8f741020634f6aaa5efbd
aman774554 <62074888+aman774554@users.noreply.github.com>
2020-03-29 12:14:43 +0530

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,25 +1,10 @@
-import time 
-import pandas as pd 
-import numpy as np 
-import matplotlib.pyplot as plt 
 import nltk 
-import io 
-import unicodedata 
-import numpy as np 
-import re 
-import string 
-from numpy import linalg 
 from nltk.sentiment.vader import SentimentIntensityAnalyzer 
 from nltk.tokenize import sent_tokenize, word_tokenize 
 from nltk.tokenize import PunktSentenceTokenizer 
-from nltk.tokenize import PunktSentenceTokenizer 
-from nltk.corpus import webtext 
 from nltk.stem.porter import PorterStemmer 
 from nltk.stem.wordnet import WordNetLemmatizer 
-
-
-with open('kindle.txt', encoding ='ISO-8859-2') as f: 
-	text = f.read() 
-	
-sent_tokenizer = PunktSentenceTokenizer(text) 
-sents = sent_tokenizer.tokenize(text) 
+file = open('kindle.txt','w') 
+p=input("Enter Sentence")
+file.write(p) 
+file.close() 
