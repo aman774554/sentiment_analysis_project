378afd82366b07d71fd42b4ea9b8cc407b195aeb
aman774554 <62074888+aman774554@users.noreply.github.com>
2020-04-01 12:14:38 +0530

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -177,3 +177,27 @@ def aspectAnalysis(df, output=False):
     if not output:
         Y = df.iloc[:, 4].values
     return df, X, Y
+
+def gaussianNaiveBayes(X, Y):
+
+    # Split in train and test
+    xTrain, xTest, yTrain, yTest = train_test_split(X, Y, test_size=0.25, random_state=0)
+    classifier = GaussianNB()
+    classifier.fit(xTrain, yTrain)
+
+    # Make predictions
+    yPred = classifier.predict(xTest)
+
+    # Confusion Matrix and accuracy
+    matrix = confusion_matrix(y_true=yTest, y_pred=yPred)
+    accuracy = accuracy_score(yPred, yTest)
+
+    # Precision, Recall and F-Score
+    fScore = f1_score(yTest, yPred, average="macro")
+    precision = precision_score(yTest, yPred, average="macro")
+    recall = recall_score(yTest, yPred, average="macro")
+    report = classification_report(yTest, yPred)
+
+
+    return [matrix, accuracy, fScore, precision, recall, report]
+
